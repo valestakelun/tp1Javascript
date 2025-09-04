9604d70b2d571dbb278d37c5ef4108eabf176a52
ValeS <valeriastakelun@gamail.com>
2025-09-03 23:02:07 -0300

    realice ejercicio 08

--- js/ejercicio-08.js
+++ js/ejercicio-08.js
@@ -1,1 +1,6 @@
 //Escribe un programa que pida un número y diga si es divisible por 2
+const numero = parseInt(prompt('Ingresa un número:'));
+if (numero % 2 === 0) {
+    document.writeln('El número ' + numero + ' es divisible por 2');
+
+} 
