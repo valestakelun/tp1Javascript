8974c8ec330f2dcab4165bfe1e7d80221d10066b
ValeS <valeriastakelun@gamail.com>
2025-09-03 23:20:53 -0300

    realice ejercicio 10 y 11

--- js/ejercicio-10.js
+++ js/ejercicio-10.js
@@ -1,1 +1,5 @@
 //Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7, sólo hay que comprobar si lo es por uno de los cuatro
+const numero = parseInt(prompt('Ingresa un número:'));
+if (numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0) {
+    document.writeln('El número ' + numero + ' es divisible por uno de estos numeros primos 2, 3, 5 o 7');
+    }
--- js/ejercicio-11.js
+++ js/ejercicio-11.js
@@ -1,1 +1,16 @@
 //Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible, hay que decir todos por los que es divisible
+const numero = parseInt(prompt('Ingresa un número:'));
+let mensaje = 'El número ' + numero + ' es divisible por: ';
+let esDivisible = false;
+if (numero % 2 === 0) {
+    document.writeln('El número ' + numero + ' es divisible por 2 <br>');
+}
+if (numero % 3 === 0) {
+    document.writeln('El número ' + numero + ' es divisible por el numero 3 <br>');
+}
+if (numero % 5 === 0) {
+    document.writeln('El número ' + numero + ' es divisible por 5 <br>');
+}
+if (numero % 7 === 0) {
+    document.writeln('El número ' + numero + ' es divisible por 7 <br>');
+}
