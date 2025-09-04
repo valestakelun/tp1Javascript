dc208ee5d4cdfbeea32f4ce2759d6cf05863044e
ValeS <valeriastakelun@gamail.com>
2025-09-03 22:48:56 -0300

    realice ejercicio 05

--- js/ejercicio-05.js
+++ js/ejercicio-05.js
@@ -1,1 +1,4 @@
 //Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+const numero1 = parseFloat(prompt('Ingresa el primer número:'));
+const numero2 = parseFloat(prompt('Ingresa el segundo número:'));
+document.writeln('El resultado de sumar estos dos numeros es: ' + (numero1 + numero2));
