f1473f31c3cf9f0683606227bcdaf868681f0295
ValeS <valeriastakelun@gamail.com>
2025-09-03 22:47:01 -0300

    realice ejercicio 04

--- js/ejercicio-04.js
+++ js/ejercicio-04.js
@@ -1,1 +1,4 @@
 //Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+const nombre = prompt('Ingresa su nombre:');
+document.writeln('Hola ' + nombre);
